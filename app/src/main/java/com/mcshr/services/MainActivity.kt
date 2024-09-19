9640e6255d1c56d6971593ea13388897dcbc9221
mcshr <hruzdovmaksym@gmail.com>
2024-09-19 19:30:53 +0300

    test service + notification

---
+++ app/src/main/java/com/mcshr/services/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.mcshr.services
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mcshr.services.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.simpleService.setOnClickListener {
+            startService(TestService.newIntent(this))
+        }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                ID_CHANNEL_NOTIFICATION_SERVICE1,
+                NAME_CHANNEL_NOTIFICATION_SERVICE1,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, ID_CHANNEL_NOTIFICATION_SERVICE1)
+            .setContentTitle("Service 1")
+            .setContentText("hi-hi ha-ha he-he")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+        private const val ID_CHANNEL_NOTIFICATION_SERVICE1 = "id1"
+        private const val NAME_CHANNEL_NOTIFICATION_SERVICE1 = "Service №1"
+    }
+
+}
---
+++ app/src/main/java/com/mcshr/services/TestService.kt
@@ -0,0 +1,51 @@
+package com.mcshr.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class TestService: Service(){
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        log("onCreate")
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        coroutineScope.launch {
+            for(i in 0..100){
+                delay(1000)
+                log("timer:$i")
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        coroutineScope.cancel()
+        log("onDestroy")
+        super.onDestroy()
+    }
+
+    private fun log(message:String){
+        Log.d("SERVICE", message)
+    }
+    companion object{
+        fun newIntent(context: Context): Intent {
+            return Intent(context, TestService::class.java)
+        }
+    }
+}
